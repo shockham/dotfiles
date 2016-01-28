66918a10aa98479c482ecfdb880107abc5ebdc59
shockham <samuel.hockham@gmail.com>
2016-01-28 11:59:49 +0000

    added newsbeuter config

--- link.sh
+++ link.sh
@@ -4,3 +4,5 @@ DOTFILE_PATH=~/Code/dotfiles
 ln -sf $DOTFILE_PATH/.vimrc ~/.vimrc
 ln -sf $DOTFILE_PATH/.tmux.conf ~/.tmux.conf
 ln -sf $DOTFILE_PATH/.zshrc ~/.zshrc 
+ln -sf $DOTFILE_PATH/.newsbeuter/urls ~/.newsbeuter/urls 
+ln -sf $DOTFILE_PATH/.newsbeuter/config ~/.newsbeuter/config 
