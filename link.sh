372e9c6763398cc7b6cd57da3b78ac0b4322cd32
shockham <samuel.hockham@gmail.com>
2017-02-10 15:11:20 +0000

    when symlinking add the required vim dirs

--- link.sh
+++ link.sh
@@ -7,3 +7,8 @@ ln -sf $DOTFILE_PATH/.zshrc ~/.zshrc
 ln -sf $DOTFILE_PATH/.newsbeuter/urls ~/.newsbeuter/urls 
 ln -sf $DOTFILE_PATH/.newsbeuter/config ~/.newsbeuter/config 
 ln -sf $DOTFILE_PATH/scripts ~/shk_scripts
+
+# make some directories for vim
+mkdir -p ~/.vim/backup
+mkdir -p ~/.vim/swap
+mkdir -p ~/.vim/undo
