524b7c07a04b110173424f2eb92578c0df28cad9
shockham <samuel.hockham@gmail.com>
2017-02-23 21:34:24 +0000

    more portable link script

--- link.sh
+++ link.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
-
 DOTFILE_PATH=~/Code/dotfiles
+
+# link main dotfiles
 ln -sf $DOTFILE_PATH/.vimrc ~/.vimrc
 ln -sf $DOTFILE_PATH/.tmux.conf ~/.tmux.conf
 ln -sf $DOTFILE_PATH/.zshrc ~/.zshrc 
+ln -sf $DOTFILE_PATH/scripts ~/shk_scripts
+
+# newsbeuter
+mkdir -p ~/.newsbeuter
 ln -sf $DOTFILE_PATH/.newsbeuter/urls ~/.newsbeuter/urls 
 ln -sf $DOTFILE_PATH/.newsbeuter/config ~/.newsbeuter/config 
-ln -sf $DOTFILE_PATH/scripts ~/shk_scripts
 
 # make some directories for vim
 mkdir -p ~/.vim/backup
