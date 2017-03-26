d2409e9e06208d99520510df6992b3fbfedc5dcd
shockham <samuel.hockham@gmail.com>
2017-03-26 19:24:46 +0100

    Added a setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+# lower case platform string
+platform="$(uname | tr '[:upper:]' '[:lower:]')"
+
+# install main tools
+if [[ $platform == 'darwin' ]]; then
+    brew install tmux vim newsbeuter
+fi
+
+# install oh-my-zsh, vundle and tpm
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm 
+
+vim +PluginInstall +qall
+tmux source ~/.tmux.conf
