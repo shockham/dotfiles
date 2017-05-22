db7f90102b55e543d50acdd8e7b926693f33fc25
shockham <samuel.hockham@gmail.com>
2017-05-22 20:38:53 +0100

    Fix where plugins are installed and conf reloaded, as not linked when setup it run setup

--- link.sh
+++ link.sh
@@ -3,16 +3,21 @@ DOTFILE_PATH=~/Code/dotfiles
 # link main dotfiles
 ln -sf $DOTFILE_PATH/.vimrc ~/.vimrc
 ln -sf $DOTFILE_PATH/.tmux.conf ~/.tmux.conf
-ln -sf $DOTFILE_PATH/.zshrc ~/.zshrc 
+ln -sf $DOTFILE_PATH/.zshrc ~/.zshrc
 ln -sf $DOTFILE_PATH/scripts ~/shk_scripts
 ln -sf $DOTFILE_PATH/.alacritty.yml ~/.alacritty.yml
 
 # newsbeuter
 mkdir -p ~/.newsbeuter
-ln -sf $DOTFILE_PATH/.newsbeuter/urls ~/.newsbeuter/urls 
-ln -sf $DOTFILE_PATH/.newsbeuter/config ~/.newsbeuter/config 
+ln -sf $DOTFILE_PATH/.newsbeuter/urls ~/.newsbeuter/urls
+ln -sf $DOTFILE_PATH/.newsbeuter/config ~/.newsbeuter/config
 
 # make some directories for vim
 mkdir -p ~/.vim/backup
 mkdir -p ~/.vim/swap
 mkdir -p ~/.vim/undo
+
+# install vim plugins
+vim +PluginInstall +qall
+# reload tmux conf
+tmux source ~/.tmux.conf
