6014d123a68d5033bae95c997704a6380d8380bd
shockham <samuel.hockham@gmail.com>
2018-05-18 14:21:40 +0100

    Remove vundle from setup and add auto install of plug to .vimrc

--- setup.sh
+++ setup.sh
@@ -15,7 +15,6 @@ fi
 
 # install oh-my-zsh, vundle and tpm
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 ./link.sh
