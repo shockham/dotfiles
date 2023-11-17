529c7ab8d3c582bc08bd8f379810ec8c5b96c876
shockham <samuel.hockham@gmail.com>
2023-11-17 11:25:48 +0000

    fix link.sh, remove setup.sh in favour of links in README

--- README.md
+++ README.md
@@ -2,17 +2,31 @@
 
 ## Usage
 
-To install the dependencies:
-```
-./setup.sh
-```
-To symlink all the confs and install vim plugins:
+To symlink all the configurations:
 ```
 ./link.sh
 ```
 
-Requires:
-- tmux
-- zsh
-- newsbeuter
-- [oh my zsh](https://github.com/robbyrussell/oh-my-zsh)
+## Uses:
+
+### Terminal
+- [tmux](https://github.com/tmux/tmux)
+- [zsh](https://www.zsh.org/) + [oh-my-zsh](https://github.com/ohmyzsh/ohmyzsh)
+- [alacritty](https://github.com/alacritty/alacritty) or [wezterm](https://github.com/wez/wezterm)
+
+# Text editor
+- [nvim](https://github.com/neovim/neovim)
+- [vim-plug](https://github.com/junegunn/vim-plug)
+
+### RSS
+- [newsboat](https://github.com/newsboat/newsboat)
+
+### IRC
+- [irssi](https://github.com/irssi/irssi)
+
+# Desktop
+- [sway](https://github.com/swaywm/sway) or [i3](https://github.com/i3/i3)
+- [rofi](https://github.com/davatorium/rofi)
+
+# Music
+- [cmus](https://github.com/cmus/cmus)
--- link.sh
+++ link.sh
@@ -7,13 +7,9 @@ ln -sf $DOTFILE_PATH/.zshrc ~/.zshrc
 ln -sf $DOTFILE_PATH/.zsh_custom ~/.zsh_custom
 ln -sf $DOTFILE_PATH/scripts ~/shk_scripts
 ln -sf $DOTFILE_PATH/.alacritty.yml ~/.alacritty.yml
-ln -sf $DOTFILE_PATH/.profile ~/.profile
 
 # newsbeuter
-mkdir -p ~/.newsbeuter
 mkdir -p ~/.newsboat
-ln -sf $DOTFILE_PATH/.newsbeuter/urls ~/.newsbeuter/urls
-ln -sf $DOTFILE_PATH/.newsbeuter/config ~/.newsbeuter/config
 ln -sf $DOTFILE_PATH/.newsbeuter/urls ~/.newsboat/urls
 ln -sf $DOTFILE_PATH/.newsbeuter/config ~/.newsboat/config
 
@@ -22,14 +18,8 @@ mkdir -p ~/.vim/backup
 mkdir -p ~/.vim/swap
 mkdir -p ~/.vim/undo
 
-# install vim plugins
-vim +PluginInstall +qall
 # reload tmux conf
 tmux source ~/.tmux.conf
-
-# mpd
-mkdir -p ~/.config/mpd
-ln -sf $DOTFILE_PATH/mpd.conf ~/.config/mpd/mpd.conf
 
 # i3
 ln -sf $DOTFILE_PATH/i3 ~/.config/
@@ -42,9 +32,6 @@ ln -sf $DOTFILE_PATH/rofi ~/.config/
 
 # irssi
 ln -sf $DOTFILE_PATH/.irssi ~
-
-# create file for SQHELL conns
-touch ~/.db_connections.vim
 
 # cmus themes
 ln -sf $DOTFILE_PATH/.cmus ~
